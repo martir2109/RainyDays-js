11c720f24921f212215180bc6338a00a51f42479
Martine R Karlsen <martinekar08@gmail.com>
2025-01-08 17:31:10 +0100

    navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("message");
+
+//Hamburger menu for smaller screens
+function myFunction() {
+    var x = document.getElementById("topnavbar");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
