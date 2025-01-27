335aaaf47962ed0d30de5d47ac85df29295bb24e
Martine R Karlsen <martinekar08@gmail.com>
2025-01-27 14:08:17 +0100

    added a loaded icon from module 7 however it does not hide when finished loading...

--- js/script.js
+++ js/script.js
@@ -8,3 +8,50 @@ function myFunction() {
       x.className = "nav";
     }
   }
+
+  //This is the loader to show the user that the application is loading
+  // look at module 7 - user experience
+  function showLoader() {
+    const loader = document.querySelector('.loader');
+    loader.hidden = false;
+  }
+  
+  function hideLoader() {
+    const loader = document.querySelector('.loader');
+    loader.hidden = true;
+  }
+  
+  export default { show: showLoader, hide: hideLoader };
+
+  import loader from './loader.js';
+import { getPosts } from './api.js';
+import { renderPosts } from './render.js';
+
+async function app() {
+  loader.show();
+  const posts = await getPosts();
+  renderPosts(posts);
+  loader.hide();
+}
+    app();
+
+
+
+//error handling
+//UI functions
+
+async function app() {
+    loader.show();
+    try {
+      const posts = await getPosts();
+      renderPosts(posts);
+    } catch (error) {
+      alert(error);
+    } finally {
+      loader.hide();
+    }
+  }
+  
+  app();
+
+  
